a840827f3d9821bd172210d4ee9b9f7f19104722
Tianyi Ma <tn.ma@outlook.com>
2017-11-29 16:54:41 -0600

    No more duplicate molecules

--- src/main/java/test/ChemExtractor.java
+++ src/main/java/test/ChemExtractor.java
@@ -6,8 +6,11 @@
 package test;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -46,11 +49,19 @@ public static void main(String[] args) {
             // NodeList in the sentence
             Element eElement = (Element) nNode;
             NodeList nodes = eElement.getChildNodes();
+
+
+            List<String> molecules = new LinkedList<String>();
                 
             for(int node_id = 0; node_id < nodes.getLength(); node_id++){
                     
                     
-                getContent(nodes.item(node_id));
+                getContent(nodes.item(node_id), molecules);
+            }
+
+            // Non duplicate molecules stored in molecule
+            for (String molecule : molecules) {
+                System.out.println(molecule);
             }
          }
       } catch (Exception e) {
@@ -59,15 +70,18 @@ public static void main(String[] args) {
    }
     
     
-    public static void getContent(Node node){
+    public static void getContent(Node node, List<String> molecules){
         if(node.getNodeName().equals("MOLECULE")){
-            System.out.println(node.getTextContent());
+            //System.out.println(node.getTextContent());
+            if (!molecules.contains(node.getTextContent())) {
+                molecules.add(node.getTextContent());
+            }
         }
         
         if(node.getChildNodes().getLength() != 0){
 
             for(int i = 0; i < node.getChildNodes().getLength(); i++){
-                getContent(node.getChildNodes().item(i));                       
+                getContent(node.getChildNodes().item(i), molecules);
             }          
         }
     }
